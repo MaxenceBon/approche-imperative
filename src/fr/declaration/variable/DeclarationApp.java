476fed3d26978ac81d9bbf9983592e3026be202b
Maxence Bon <maxence.bon@orange.fr>
2022-10-06 16:36:43 +0200

    DeclarationApp

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,34 @@
+package fr.declaration.variable;
+
+import javax.swing.text.GapContent;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 2;
+		short b = 4;
+		int c = 45;
+		long d = 236L;
+		float e = 1563F;
+		double f = 652135674D;
+		char g = 'g';
+		boolean booleanTest = true;
+		String test = "hello";
+		
+		String randomString="Voici le résultat d'un calcul :\n"+"1+5=6";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(booleanTest);
+		System.out.println(test);
+		
+		System.out.println(randomString);
+	}
+
+}
